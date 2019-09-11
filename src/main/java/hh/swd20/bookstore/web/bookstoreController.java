869fa64de0b3be400282ca1f1db8333d8f283c15
Intomies <valtteri.nummi@gmail.com>
2019-09-11 15:58:21 +0300

    Did the things in Tehtava 3.2, added booklist.html and modified bookstoreController to list all the books from database.

--- src/main/java/hh/swd20/bookstore/web/bookstoreController.java
+++ src/main/java/hh/swd20/bookstore/web/bookstoreController.java
@@ -1,17 +1,37 @@
 package hh.swd20.bookstore.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+import hh.swd20.bookstore.domain.Book;
+import hh.swd20.bookstore.domain.BookRepository;
 
 
 @Controller
 
 public class bookstoreController {
 	
+	@Autowired
+	BookRepository bookRepository;
+	
 	@GetMapping("/index")
 	public String sayHello() {
 		return "bookstoreindex";
 
+	}
+	
+	@GetMapping("/books")
+	public String getBooks(Model model) {
+		List<Book> books = (List<Book>) bookRepository.findAll();
+		model.addAttribute("books", books);
+		return "booklist";
+		
 	}
 
 }
