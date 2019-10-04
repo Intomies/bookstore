1988b515532932460bf6695d7f986cea460162c4
Intomies <valtteri.nummi@gmail.com>
2019-10-04 09:15:59 +0300

    Did the things in 6.2 and added some styles including padding and navbar.

--- src/main/java/hh/swd20/bookstore/BookstoreApplication.java
+++ src/main/java/hh/swd20/bookstore/BookstoreApplication.java
@@ -4,6 +4,8 @@ import hh.swd20.bookstore.domain.Book;
 import hh.swd20.bookstore.domain.BookRepository;
 import hh.swd20.bookstore.domain.Category;
 import hh.swd20.bookstore.domain.CategoryRepository;
+import hh.swd20.bookstore.domain.User;
+import hh.swd20.bookstore.domain.UserRepository;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,30 +17,39 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class BookstoreApplication {
 
-	private static final Logger log = LoggerFactory.getLogger(BookstoreApplication.class);  // uusi loggeriattribuutti
-	
+	private static final Logger log = LoggerFactory.getLogger(BookstoreApplication.class); // uusi loggeriattribuutti
+
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
-@Bean
-public CommandLineRunner carDemo(BookRepository bookRepository, CategoryRepository catRepository) { 
-	return (args) -> {
-		log.info("create categories");
-		Category adventure = new Category("Adventure");
-		catRepository.save(adventure);
-		Category science = new Category("Science");
-		catRepository.save(science);
-		
-		log.info("save a couple of books");
-		bookRepository.save(new Book("Suuri Seikkailu", "Kari Kirjailija", "978-951-98548-9-2", 2016, 19.90, adventure));
-		bookRepository.save(new Book("Pieni Seikkailu", "Kari Kirjailija", "978-951-98548-9-3", 2000, 29.90, adventure));	
-		bookRepository.save(new Book("Uutta Tietoa", "Viisas Tietäjä", "978-951-98548-9-4", 2018, 15.95, science));
-		
-		log.info("fetch all books");
-		for (Book book : bookRepository.findAll()) {
-			log.info(book.toString());
-		}
+	@Bean
+	public CommandLineRunner carDemo(BookRepository bookRepository, CategoryRepository catRepository,
+			UserRepository uRepository) {
+		return (args) -> {
+			log.info("create categories");
+			Category adventure = new Category("Adventure");
+			catRepository.save(adventure);
+			Category science = new Category("Science");
+			catRepository.save(science);
+
+			log.info("save a couple of books");
+			bookRepository
+					.save(new Book("Suuri Seikkailu", "Kari Kirjailija", "978-951-98548-9-2", 2016, 19.90, adventure));
+			bookRepository
+					.save(new Book("Pieni Seikkailu", "Kari Kirjailija", "978-951-98548-9-3", 2000, 29.90, adventure));
+			bookRepository.save(new Book("Uutta Tietoa", "Viisas Tietäjä", "978-951-98548-9-4", 2018, 15.95, science));
+
+			// Create users: admin/admin user/user
+			uRepository.save(new User("user", "user@useremail.com",
+					"$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER"));
+			uRepository.save(new User("admin", "admin@bookstore.com",
+					"$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN"));
+
+			log.info("fetch all books");
+			for (Book book : bookRepository.findAll()) {
+				log.info(book.toString());
+			}
 
 		};
 	}
--- src/main/java/hh/swd20/bookstore/web/bookstoreController.java
+++ src/main/java/hh/swd20/bookstore/web/bookstoreController.java
@@ -1,6 +1,7 @@
 package hh.swd20.bookstore.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -50,24 +51,28 @@ public class bookstoreController {
 	}
 	
 	//Restful get all books
+	@PreAuthorize("hasAuthority('ADMIN')")
 	@GetMapping("/books")
 	public @ResponseBody List<Book> getBooksRest() {
 		return (List<Book>) bookRepository.findAll();
 	}
 	
 	//Restful get book by id
+	@PreAuthorize("hasAuthority('ADMIN')")
 	@GetMapping("books/{id}")
 	public @ResponseBody Optional<Book> getBookRest(@PathVariable("id") Long bookId) {
 		return bookRepository.findById(bookId);
 	}
 	
 	//Restful save new book
+	@PreAuthorize("hasAuthority('ADMIN')")
 	@PostMapping("/books")
 	public @ResponseBody Book saveBookRest(@RequestBody Book book) {
 		return bookRepository.save(book);
 	}
 	
 	//Tyhjä lomake
+	@PreAuthorize("hasAuthority('ADMIN')")
 	@GetMapping("/newbook")
 	public String getEmptyBookForm(Model model) {
 	model.addAttribute("book", new Book());
@@ -77,23 +82,26 @@ public class bookstoreController {
 	}
 	
 	//Lomakkeen tietojen vastaanotto ja tallentaminen
+	@PreAuthorize("hasAuthority('ADMIN')")
 	@PostMapping("/newbook")
 	public String saveBook(@ModelAttribute Book book) {
 		bookRepository.save(book);
-		return "redirect:/books";
+		return "redirect:/booklist";
 		
 	}
 	
 	//Kirjan poistaminen
+	@PreAuthorize("hasAuthority('ADMIN')")
 	@GetMapping("/deletebook/{id}")
 	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
 		bookRepository.deleteById(bookId);
-		return "redirect:../books";
+		return "redirect:../booklist";
 		
 	}
 	
 	//Kirjan muokkaaminen
-	@RequestMapping(value = "/editbook/{id}")
+	@PreAuthorize("hasAuthority('ADMIN')")
+	@RequestMapping(value="/editbook/{id}")
 	public String editBook(@PathVariable("id") Long bookId, Model model) {
 		model.addAttribute("book", bookRepository.findById(bookId));
 		model.addAttribute("categories", catrepository.findAll());
